54cfbf6a1424624dd82ba2e0f8865d96cbbcbef8
Moharnab Saikia <moharnab123saikia@gmail.com>
2016-02-28 20:11:57 -0500

    fixed fractional stars display on review page

--- webscrapper/frontend/webscrapper_app/app/assets/javascripts/application.js
+++ webscrapper/frontend/webscrapper_app/app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ $.fn.stars = function() {
         // Get the value
         var val = parseFloat($(this).html());
         // Make sure that the value is in 0 - 5 range, multiply to get width
-        var size = Math.max(0, (Math.min(5, val))) * 8;
+        var size = Math.max(0, (Math.min(5, val))) * 16;
         // Create stars holder
         var $span = $('<span />').width(size);
         // Replace the numerical value with stars
