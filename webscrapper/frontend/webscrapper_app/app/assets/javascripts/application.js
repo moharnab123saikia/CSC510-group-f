666caffd3449223cf4db761df1972f1665bdac42
Moharnab Saikia <moharnab123saikia@gmail.com>
2016-02-28 20:00:37 -0500

    fix stars display issue

--- webscrapper/frontend/webscrapper_app/app/assets/javascripts/application.js
+++ webscrapper/frontend/webscrapper_app/app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ $.fn.stars = function() {
         // Get the value
         var val = parseFloat($(this).html());
         // Make sure that the value is in 0 - 5 range, multiply to get width
-        var size = Math.max(0, (Math.min(5, val))) * 16;
+        var size = Math.max(0, (Math.min(5, val))) * 8;
         // Create stars holder
         var $span = $('<span />').width(size);
         // Replace the numerical value with stars
@@ -27,9 +27,9 @@ $.fn.stars = function() {
     });
 }
 
-$(function() {
-    $('span.stars').stars();
-});
+// $(function() {
+//     $('span.stars').stars();
+// });
 
 $.fn.stars10 = function() {
     return $(this).each(function() {
@@ -44,7 +44,7 @@ $.fn.stars10 = function() {
     });
 }
 
-$(function() {
-    $('span.stars10').stars10();
-});
+// $(function() {
+//     $('span.stars10').stars10();
+// });
 
